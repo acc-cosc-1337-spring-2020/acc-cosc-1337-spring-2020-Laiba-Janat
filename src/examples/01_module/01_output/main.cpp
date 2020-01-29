e6261f66736c55b4df6832df4985c4600b29a718
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-01-28 22:40:39 -0600

    Fix example errors

--- src/examples/01_module/01_output/main.cpp
+++ src/examples/01_module/01_output/main.cpp
@@ -1,17 +1,11 @@
-#include"vars.h"// use file i created 
+#include "output.h"
+#include<iostream>
 
-
-
+using std::cout;
 
 int main() 
 {
-    display_number(5);
-	
-	int num = 5;
-
-	
-
-	cout << square_number(num);
+	cout<<"Helllo world!";
 
 	return 0;
 }
