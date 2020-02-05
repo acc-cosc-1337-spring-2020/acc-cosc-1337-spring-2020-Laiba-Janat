0a1b67fb84137dda52fe45abeaf316da0c6c5a51
9418914 <laiba.janat@austincc.edu>
2020-02-04 19:17:03 -0600

    get generation changes

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "if.h"
+#include "if_else.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -8,6 +9,20 @@ TEST_CASE("Verify Test Configuration", "verification") {
 TEST_CASE("Test is even Function")
 {
 	REQUIRE (is_even (2)== true);
-	
 	REQUIRE(is_even(3) == false);
+
 }
+TEST_CASE("Test get generation function")
+{
+	REQUIRE(get_generation(1998) == "Centenial");
+	REQUIRE(get_generation(1977) == "Millenial");
+	REQUIRE(get_generation(1965) == "Generation X");
+	REQUIRE(get_generation(1946) == "Baby Boomer");
+	REQUIRE(get_generation(1925) == "Silent Generation");
+	REQUIRE(get_generation(1000) == "Invalid Year");
+
+
+	
+}
+
+
