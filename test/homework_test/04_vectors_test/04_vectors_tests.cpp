f0f2a86151e9b33a0c6d1cd6b2711f8d734e5aa8
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-29 17:55:57 -0600

    hw4 grading

--- test/homework_test/04_vectors_test/04_vectors_tests.cpp
+++ test/homework_test/04_vectors_test/04_vectors_tests.cpp
@@ -8,9 +8,12 @@ TEST_CASE("Verify Test Configuration", "verification") {
 }
 
 TEST_CASE("test Get Max function") {
-	REQUIRE(get_max_from_vector (3,8,1,99,1000)== 1000);
-	REQUIRE(get_max_from_vector(15, 12, 11, 99, 88) == 99);
-	REQUIRE(get_max_from_vector(150, 120, 11, 990, 88888) == 88888);
+	std::vector<int> nums{ 3,8,1,99,1000 };
+	REQUIRE(get_max_from_vector (nums)== 1000);
+	nums = { 15, 12, 11, 99, 88 };
+	REQUIRE(get_max_from_vector(nums) == 99);
+	nums = { 150, 120, 11, 990, 88888 };
+	REQUIRE(get_max_from_vector(nums) == 88888);
 }
 TEST_CASE("test is_prime function") {
 	REQUIRE(is_prime(2) == true);
@@ -20,8 +23,10 @@ TEST_CASE("test is_prime function") {
 }
 
 TEST_CASE("test vector_of_primes function") {
-	REQUIRE(vector_of_primes(10) == 2, 3, 5, 7);
-	REQUIRE(vector_of_primes(50) == 2, 3, 5, 7, 11, 13 17, 19, 23, 29, 31, 37, 41, 43, 47);
+	std::vector<int> nums = { 2, 3, 5, 7 };
+	REQUIRE(vector_of_primes(10) == nums);
+	nums = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+	REQUIRE(vector_of_primes(50) == nums);
 }
 
 	
